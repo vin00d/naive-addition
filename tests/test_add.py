0647e0c00f70054bd71c57bef2bcb8a2a1b0a6b6
Vinod <vinod@corazonlabs.ai>
2022-05-12 13:24:51 -0700

    removed type hints on add function, added more tests

--- tests/test_add.py
+++ tests/test_add.py
@@ -5,4 +5,7 @@ def test_add():
     assert add(1) == 101
 
 def test_add_np():
-    assert add(np.array([1])) == np.array([101])
+    assert (add(np.array([1, 2])) == [101, 102]).all()
+
+def test_add_np2():
+    assert (add(np.array([1]), np.array([3])) == [4]).all()
